2ebb1c33cbc2df6e756e6f89c18f764b63d711ad
AndrewMcDonnell <andrew.mc.donnell@me.com>
2021-02-23 10:42:48 +0000

    Second commit - added some formula to remove whitespace from December.

--- Tax.py
+++ Tax.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 #Importing CSV
 tax = pd.read_csv('Open Data Tax Receipts Source.csv')
 
@@ -18,15 +17,20 @@ tax_cols = pd.read_csv('Open Data Tax Receipts Source.csv', names=cols)
 print(tax_cols.head())
 print(tax_cols.shape)
 
-#This removes the whitespace after December so the full date column fills out
-tax_cols['MonthLong'] = tax_cols['MonthLong'].apply(lambda x: x.strip())
-
 #Adding the last day of each month
 date = pd.to_datetime(tax_cols['Year'].astype(str) + tax_cols['MonthLong'], format='%Y%B', errors='coerce')
 tax_cols['FullDate'] = date + pd.offsets.MonthEnd()
 
-#After looking at file it turns out December has a few blank spaces after it.
-print(tax_cols.head())
-print(tax_cols.isna().any())
+#Checking for errors
 print(tax_cols.isna().sum())
 
+#Checked date set and there is some shitespace after 'December' which is removed by this
+tax_cols['MonthLong'] = tax_cols['MonthLong'].apply(lambda x: x.strip())
+
+#Running this again to ensure the FullDate column is filled in full
+date = pd.to_datetime(tax_cols['Year'].astype(str) + tax_cols['MonthLong'], format='%Y%B', errors='coerce')
+tax_cols['FullDate'] = date + pd.offsets.MonthEnd()
+
+#After looking at file it turns out December has a few blank spaces after it.
+print(tax_cols.isna().sum())
+print(tax_cols.head())
