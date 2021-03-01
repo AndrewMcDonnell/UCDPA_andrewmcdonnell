16240171bca4d7dfa86754e79d787b927e701305
AndrewMcDonnell <andrew.mc.donnell@me.com>
2021-03-01 19:02:35 +0000

    Fifth commit - removed some elements and fixed the tax heads short error

--- Tax.py
+++ Tax.py
@@ -5,6 +5,8 @@ import datetime as dt
 
 #Importing CSV
 tax = pd.read_csv('Open Data Tax Receipts Source.csv')
+live_reg = pd.read_csv('Live Register.csv')
+#pd.set_option('display.max_columns',50)
 
 #Checking CSV
 print(tax.head())
@@ -18,32 +20,55 @@ tax_cols = pd.read_csv('Open Data Tax Receipts Source.csv', names=cols)
 print(tax_cols.head())
 print(tax_cols.shape)
 
-#Adding the last day of each month
+#Amending the date so it shows as the last day of each month
 date = pd.to_datetime(tax_cols['Year'].astype(str) + tax_cols['MonthLong'], format='%Y%B', errors='coerce')
 tax_cols['FullDate'] = date + pd.offsets.MonthEnd()
 
-#Checking for errors
+#Checking for errors brings up 510 errors
 print(tax_cols.isna().sum())
 
-#Checked date set and there is some shitespace after 'December' which is removed by this
+#Checked the data set and there is some whitespace after 'December  ' which is removed by this
 tax_cols['MonthLong'] = tax_cols['MonthLong'].apply(lambda x: x.strip())
 
 #Running this again to ensure the FullDate column is filled in full
 date = pd.to_datetime(tax_cols['Year'].astype(str) + tax_cols['MonthLong'], format='%Y%B', errors='coerce')
 tax_cols['FullDate'] = date + pd.offsets.MonthEnd()
 
-#After looking at file it turns out December has a few blank spaces after it.
+#Checking for errors again to ensure none are pulled through - all is ok
 print(tax_cols.isna().sum())
-print(tax_cols.head())
+
+#Create abbreviated column for ease of charting
+tax_heads = tax_cols['Tax Head'].unique()
+
+tax_heads_list = []
+
+for i in tax_heads:
+        tax_heads_list.append(i)
+
+print(tax_heads_list)
+
+tax_dict = {'Tax Head': tax_heads_list,
+        'Tax Heads Short': ['CUST','EXD','CGT','SD','IT','CORP','VAT','TEL','MOT','UNA','CAT','LPT']}
+
+tax_dict_pd = pd.DataFrame(data=tax_dict)
+print(tax_dict_pd)
+
+tax_cols_short = tax_cols.merge(tax_dict_pd, on='Tax Head', how='left')
+print(tax_cols_short)
 
 #Looking at live register data
-live_reg = pd.read_csv('Live Register.csv')
+print(live_reg.head())
 
-#Reformatting the Month column so I can match it with the tax info
+#Adding a full date column to the live reg info so I can match the tax table with live reg table
 date_live_reg = pd.to_datetime(live_reg['Month'], format='%YM%M')
 live_reg['FullDate'] = date_live_reg + pd.offsets.MonthEnd()
 
-print(live_reg.head())
+live_reg['NewYear'] = live_reg['FullDate'].dt.strftime('%Y')
+
+print(live_reg.head)
+
+#Checking live reg table for errors - a lot in the value column - can't figure out how to plot
+print(live_reg.isna().sum())
 
 #Creating a maak filter that just contains all ages, both sexes, all classes
 filt = ((live_reg['Age Group'] == 'All ages') & (live_reg['Sex'] == 'Both sexes') & (live_reg['Social Welfare Scheme'] == 'All classes'))
@@ -53,12 +78,12 @@ live_reg_merge = live_reg[filt]
 print(live_reg_merge)
 
 #Merging the tables
-tax_live_reg_merge = pd.merge_asof(tax_cols, live_reg_merge, on='FullDate', direction='forward')
+tax_live_reg_merge = pd.merge_asof(tax_cols_short, live_reg_merge, on='FullDate', direction='forward')
 print(tax_live_reg_merge)
 
+#Creating a filter to remove the projected figures
 actual_filt = (tax_live_reg_merge['Actual/Projected'] == 'Actual Outturn')
 tax_live_reg_actual = tax_live_reg_merge[actual_filt]
-
 
 #Tables - need to adjust for profile v actual outturn in tax
 total_tax_live_reg_actual = (tax_live_reg_actual.groupby(['Year'])['Amount (000s)'].sum())
@@ -66,4 +91,6 @@ total_tax_live_reg_actual.plot(kind='bar', color='limegreen', width=0.6)
 plt.xlabel('Year')
 plt.ylabel('Tax Revenue in €bn')
 plt.title('Tax Revenue per Year')
-plt.show()
+#plt.show()
+
+print(tax_live_reg_actual.columns)
