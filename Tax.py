1a9f15689707eb2731aaf71076e6d097deead8bf
AndrewMcDonnell <andrew.mc.donnell@me.com>
2021-03-16 12:26:56 +0000

    Final commit

--- Tax.py
+++ Tax.py
@@ -1,44 +1,47 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import datetime as dt
 
-#Importing CSV
+#----Importing CSV----
 tax = pd.read_csv('Open Data Tax Receipts Source.csv')
 live_reg = pd.read_csv('Live Register.csv')
+unemp8315 = pd.read_csv('Unemployment 19832015.csv')
+unemp9821 = pd.read_csv('Unemployment 19982021.csv')
+unempcovid = pd.read_csv('Unemployment Covid.csv')
 
-#pd.set_option('display.max_columns',50)
+pd.set_option('display.max_columns',50)
+pd.set_option('display.max_rows',50)
 
-#Checking Tax CSV
+#----Checking Tax CSV----
 #print(tax.head())
 #print(tax.shape)
 
-#Insert column names
+#----Insert column names----
 cols = ['Year', 'MonthLong', 'Actual/Projected', 'Tax Head', 'Amount (000s)']
 tax_cols = pd.read_csv('Open Data Tax Receipts Source.csv', names=cols)
 
-#Checking dataframe post column insertion / looking for one extra row
+#----Checking dataframe post column insertion / looking for one extra row----
 #print(tax_cols.head())
 #print(tax_cols.shape)
 
-#Creating a combined date column
+#----Creating a combined date column----
 date = pd.to_datetime(tax_cols['Year'].astype(str) + tax_cols['MonthLong'], format='%Y%B', errors='coerce')
 tax_cols['FullDate'] = date + pd.offsets.MonthEnd()
 
-#Checking for errors brings up 510 errors
+#----Checking for errors brings up 510 errors----
 #print(tax_cols.isna().sum())
 
-#Checked the data set and there is some whitespace after 'December  ' which is removed by this
+#----Checked the data set and there is some whitespace after 'December  ' which is removed by this----
 tax_cols['MonthLong'] = tax_cols['MonthLong'].apply(lambda x: x.strip())
 
-#Running this again to ensure the FullDate column is filled in full. I would normally remove the date line above but keeping it as an example that I check the errors
+#----Running this again to ensure the FullDate column is filled in full. I would normally remove the date line above but keeping it as an example that I check the errors----
 date = pd.to_datetime(tax_cols['Year'].astype(str) + tax_cols['MonthLong'], format='%Y%B', errors='coerce')
 tax_cols['FullDate'] = date + pd.offsets.MonthEnd()
 
-#Checking for errors again to ensure none are pulled through - all is ok
+#----Checking for errors again to ensure none are pulled through - all is ok----
 #print(tax_cols.isna().sum())
 
-#Create abbreviated column for ease of charting
+#----Create abbreviated column for ease of charting----
 tax_heads = tax_cols['Tax Head'].unique()
 
 tax_heads_list = []
@@ -46,71 +49,147 @@ tax_heads_list = []
 for i in tax_heads:
         tax_heads_list.append(i)
 
-#Change tax collected to rounded millions
+#----Change tax collected to rounded millions----
 
 tax_cols['Amount (MM)'] = (tax_cols['Amount (000s)'] / 1000).astype(int)
 
-print(tax_heads_list)
+#print(tax_heads_list)
 
+#----Creating a short hand list for the main tax heads----
 tax_dict = {'Tax Head': tax_heads_list,
-        'Tax Heads Short': ['CUST','EXD','CGT','SD','IT','CORP','VAT','TEL','MOT','UNA','CAT','LPT']}
+        'Tax Heads Short': ['OTHER','EXD','CGT','SD','IT','CORP','VAT','OTHER','OTHER','UNA','OTHER','OTHER']}
 
 tax_dict_pd = pd.DataFrame(data=tax_dict)
-#print(tax_dict_pd)
+print(tax_dict_pd)
 
 tax_cols_short = tax_cols.merge(tax_dict_pd, on='Tax Head', how='left')
-print(tax_cols_short)
+#print(tax_cols_short)
 
-#Removing the projected rows and 2021 (not complete data)
+#----Removing the projected rows and 2021 (not complete data) and Unallocated Tax Recipts----
 
-actual_filter = (tax_cols_short['Actual/Projected'] == 'Actual Outturn') & (tax_cols_short['Year'] != 2021)
+actual_filter = (tax_cols_short['Actual/Projected'] == 'Actual Outturn') & (tax_cols_short['Year'] != 2021) & (tax_cols_short['Tax Heads Short'] != 'UNA')
 tax_final = tax_cols_short[actual_filter]
 
-print(tax_final['Actual/Projected'].unique())
-print(tax_final)
+#print(tax_final['Actual/Projected'].unique())
+#print(tax_final)
 
-#Looking at live register data
+#----Looking at live register data----
 #print(live_reg.head())
 
-#Adding a full date column to the live reg info so I can match the tax table with live reg table
+#----Adding a full date column to the live reg info so I can match the tax table with live reg table----
 date_live_reg = pd.to_datetime(live_reg['Month'], format='%YM%M')
 live_reg['FullDate'] = date_live_reg + pd.offsets.MonthEnd()
 
-live_reg['NewYear'] = live_reg['FullDate'].dt.strftime('%Y')
-
-#print(live_reg.head)
-
-#Checking live reg table for errors
+#----Checking live reg table for errors----
 
 #print(live_reg.isna().sum())
-
 #print(live_reg['VALUE'].isna().groupby(live_reg['NewYear']).sum())
 
 live_reg_clean = live_reg.dropna(axis=0)
 
-#print(live_reg_clean)
-
-#Creating a maak filter that just contains all ages, both sexes, all classes
+#----Creating a maak filter that just contains all ages, both sexes, all classes----
 live_reg_filter = ((live_reg_clean['Age Group'] == 'All ages') & (live_reg_clean['Sex'] == 'Both sexes') & (live_reg_clean['Social Welfare Scheme'] == 'All classes'))
 
-#Checking the mask works
+#----Checking the mask works----
 live_reg_final = live_reg_clean[live_reg_filter]
 
 #print(live_reg_final['VALUE'].isna().groupby(live_reg_final['NewYear']).sum())
 
-print(live_reg_final)
+#print(live_reg_final)
 
-#Merging the tables
+#----Merging the tables----
 tax_live_reg_final = pd.merge_asof(tax_final, live_reg_final, on='FullDate', direction='forward')
-print(tax_live_reg_final)
+#print(tax_live_reg_final)
 
-#Checking there are no errors
-print(tax_live_reg_final.isna().sum())
+#----Checking there are no errors----
+#print(tax_live_reg_final.isna().sum())
 
-#Tables
-tax_live_reg_chart = (tax_live_reg_final.groupby(['Year'])['Amount (MM)'].sum())
-tax_live_reg_chart.plot(kind='bar', color='limegreen', width=0.6)
+#----Creating smaller tables and dropping rows I don't need so it's easier to merge----
+unemp8315_filter = unemp8315[['Month','VALUE']]
+unemp1 = unemp8315_filter.drop(unemp8315_filter.index[180:388])
+
+unemp9821_filter = unemp9821[['Month','VALUE']]
+unemp2 = unemp9821_filter.drop(unemp9821_filter.index[266:277])
+
+unempcovid_filter = (unempcovid['Lower and Upper Bound'] == 'Upper Bound (COVID-19 Adjusted MUR)')
+unemp_covid_filter2 = (unempcovid[unempcovid_filter])
+unemp3 = unemp_covid_filter2[['Month','VALUE']]
+
+unemp_final = pd.concat([unemp1, unemp2, unemp3], axis=0)
+unemp_final.reset_index(inplace=False)
+#unemp_final.set_index('Month', inplace=True)
+#print(unemp_final)
+
+#----Merging the unemployed %----
+final_data = pd.merge(tax_live_reg_final, unemp_final, on='Month', suffixes=['_#', '_%'])
+
+print(final_data)
+
+#----Updating Unemployent numbers for 2020 to include the pandemic unemployment payment----
+
+unemp2020 = np.array([[183900,182500,209400,216900,227900,213700,226100,213700,216000,211600,203100,193700],[0,0,283129,602107,543164,438933,274578,224956,217142,329991,351424,335599]])
+
+total_unemployed_pup = (final_data.groupby(['Year'])['VALUE_#'].max())
+
+unemp2020_mean = unemp2020.max()
+
+total_unemployed_pup.iloc[-1] = unemp2020_mean
+
+#print(total_unemployed_pup)
+
+#----Charts----
+
+#----Tax chart----
+tax_live_reg_chart1 = (final_data.groupby(['Year'])['Amount (MM)'].sum())
+tax_live_reg_chart1.plot(kind='bar', color='limegreen', width=0.6)
 plt.xlabel('Year')
 plt.ylabel('Tax revenue (€ millions)')
 plt.title('Tax revenue per year')
 #plt.show()
+
+#----Unemployment Chart----
+tax_live_reg_chart2 = (final_data.groupby(['Year'])['VALUE_%'].mean())
+tax_live_reg_chart2.plot(color='limegreen', marker='o', linestyle='--')
+plt.xlabel('Year')
+plt.ylabel('Unemployment %')
+plt.title('Unemployment % per year')
+#plt.show()
+
+#----Combined tax and unemployment chart----
+time = final_data['Year'].unique()
+tax_data= (final_data.groupby(['Year'])['Amount (MM)'].sum())
+unemployment_data = (final_data.groupby(['Year'])['VALUE_%'].mean())
+
+fig, ax1 = plt.subplots()
+
+color = 'mediumseagreen'
+ax1.set_title('Irish tax revenue and unemployment trend')
+ax1.set_xlabel('Years')
+ax1.set_ylabel('Tax revenue (€ millions)')
+ax1.bar(time, tax_data, color = color)
+ax1.tick_params(axis='y', labelcolor = color)
+
+ax2 = ax1.twinx()
+
+color2 = 'coral'
+ax2.set_ylabel('Average Unemployment %')
+ax2.plot(time, unemployment_data, color=color2, marker='.', linestyle='--')
+ax2.tick_params(axis='y', labelcolor = color2)
+
+plt.show()
+
+#----unemployed barchart----
+total_unemployed_pup.plot(kind='bar', color='limegreen', width=0.6)
+plt.xlabel('Year')
+plt.ylabel('Tax revenue (€ millions)')
+plt.title('Tax revenue per year')
+plt.show()
+
+#----Tables----
+#print(final_data.groupby(['Year'])['VALUE_%'].mean())
+#print(final_data.groupby(['Year'])['VALUE_#'].mean())
+#print(final_data.groupby(['Tax Heads Short'])['Amount (MM)'].sum().sort_values(ascending=False))
+#print(final_data.groupby(['Year'])['Amount (MM)'].sum().sort_values(ascending=False))
+
+
+
--- Unemployment.py
+++ Unemployment.py
@@ -4,16 +4,9 @@ unemp8315 = pd.read_csv('Unemployment 19832015.csv')
 unemp9821 = pd.read_csv('Unemployment 19982021.csv')
 unempcovid = pd.read_csv('Unemployment Covid.csv')
 
-#pd.set_option('display.max_columns',10pd.set_option('display.max_rows',400)
+#pd.set_option('display.max_columns',10)
+#pd.set_option('display.max_rows',400)
 
-#full_unemp = pd.concat([unemp8315, unemp9821, unempcovid], axis=0)
-#clean_month_list = full_unemp['Month'].drop_duplicates()
-
-#clean_month_list2 = pd.DataFrame(data=clean_month_list, columns=['Month'])
-
-#unemp8315 1983M01 to 2015M04 - drop rows 1998M01 (180) to 2015M04 (up to 388)
-#unemp9821 1998M01 to 2021M01 - drop rows 2020M03 (266) to 2021M01 (up to 277)
-#unempcovid 2020M03 to 2021M01
 
 #Creating smaller tables and dropping rows I don't need so it's easier to merge
 unemp8315_filter = unemp8315[['Month','VALUE']]
@@ -26,14 +19,24 @@ unempcovid_filter = (unempcovid['Lower and Upper Bound'] == 'Upper Bound (COVID-
 unemp_covid_filter2 = (unempcovid[unempcovid_filter])
 unemp3 = unemp_covid_filter2[['Month','VALUE']]
 
-full = pd.concat([unemp1, unemp2, unemp3], axis=0)
-full.set_index('Month', inplace=True)
-print(full)
+unemp_final = pd.concat([unemp1, unemp2, unemp3], axis=0)
+unemp_final.reset_index(inplace=False)
+#unemp_final.set_index('Month', inplace=True)
+print(unemp_final)
 
-print(len(unemp1) + len(unemp2) + len(unemp3))
+check = (len(unemp1) + len(unemp2) + len(unemp3))
 
-#merged = pd.merge(clean_month_list2,unemp_covid_use,how='left', on='Month')
+if len(unemp_final) == check:
+    print('OK')
+else:
+    print('ERROR')
 
-#merged2 = pd.merge(merged,unemp9821_filter,how='left', on='Month')
+#Didn't use the below in the end
+#full_unemp = pd.concat([unemp8315, unemp9821, unempcovid], axis=0)
+#clean_month_list = full_unemp['Month'].drop_duplicates()
 
-#merged3= pd.merge(merged2, unemp8315_filter, how='left', on='Month')
+#clean_month_list2 = pd.DataFrame(data=clean_month_list, columns=['Month'])
+
+#unemp8315 1983M01 to 2015M04 - drop rows 1998M01 (180) to 2015M04 (up to 388)
+#unemp9821 1998M01 to 2021M01 - drop rows 2020M03 (266) to 2021M01 (up to 277)
+#unempcovid 2020M03 to 2021M01
